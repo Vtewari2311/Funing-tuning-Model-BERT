7793eeafb3c08f73e4273047a8a2eac6add9101b
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2022-01-04 16:38:24 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import helpers
+
+# Executable.py
+
+# Model 0
+print(helpers.train_test_intial_model())
+print(helpers.train_test_small_dataset_intial_model())
+
+# Model 1
+print(helpers.train_test())
+print(helpers.train_test_small_dataset())
+
+# Model 2
+print(helpers.train_test_alternate_model())
+print(helpers.train_test_small_dataset_alternate_model())
+
+# Model 3
+print(helpers.base_case_train_test())
+print(helpers.base_case_train_test_small_dataset())
+
+# Model 4
+print(helpers.epoch_train_test())
+print(helpers.epoch_train_test_small_dataset())
+
+# Model 5
+print(helpers.training_examples_train_test())
+print(helpers.training_examples_train_test_small_dataset())
